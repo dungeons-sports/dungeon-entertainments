90dcea932d1b7d192a8b192743f49a92d6a81abe
AbhijeetDash <abhijeetdash1999@gmail.com>
2024-12-28 06:12:37 +0530

    Responsive: Tablet & Desktop Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,19 +9,19 @@ export default function Home() {
 		<div className="flex flex-col">
 			{/* Section One */}
 			<div className="relative w-screen h-[90vh]">
-				<div className="absolute inset-0 bg-black opacity-80 z-10"></div>
+				<div className="absolute inset-0 bg-black opacity-80 z-10 h-[100vh]"></div>
 				<div className="absolute inset-0 flex justify-center items-center blur-lg z-0">
 					<img
 						alt="background"
 						src="/assets/background_image.png"
-						style={{ width: '100%', height: 'auto' }}
+						className="w-full sm:w-[70vw]"
 					/>
 				</div>
-				<div className="relative z-20 p-8">
+				<div className="relative z-20 p-8 lg:px-24">
 					<img alt="icon" src="/assets/Icon.png" width="60" height="60" />
 					<div className="flex flex-col items-start justify-center h-[80vh]">
-						<div className="font-pixel text-4xl pb-4">Dungeon</div>
-						<div className="pb-6">
+						<div className="font-pixel text-4xl pb-4 sm:text-6xl">Dungeon</div>
+						<div className="pb-6 sm:text-xl sm:w-[60vw]">
 							Find the best gaming arenas around you from our hand-picked places
 							for the best esports experience.
 						</div>
@@ -29,7 +29,7 @@ export default function Home() {
 							content="Get started"
 							bgColor="bg-purple-500"
 							textColor="text-white"
-							width="w-40"
+							width="w-40 sm:w-48"
 						/>
 					</div>
 				</div>
@@ -37,115 +37,113 @@ export default function Home() {
 					<img
 						alt="dragon icon"
 						src="/assets/dragon.png"
-						width="200"
-						height="200"
+						className="w-40 sm:w-72 lg:w-96"
 					/>
 				</div>
 			</div>
 
 			{/* Section Two */}
 			<div className="h-[90vh] w-screen z-20 flex items-center justify-center py-8">
-				<StackContainer width="w-[90vw]" height="h-auto">
-					<div className="relative flex flex-col justify-center items-start w-[100vw] py-5">
-						<img alt="icon" src="/assets/rocket.png" width="160" height="160" />
-
-						<div className="font-pixel text-2xl pb-2">For Players</div>
-						<div className="pb-3">
-							Find about latest games and get to experience them in the nearest
-							arena.
+				<StackContainer width="w-[90vw]" height="h-auto sm:h-[70vh]">
+					<div className="relative flex flex-col sm:flex-row justify-center items-start w-[100vw] py-5">
+						<img alt="icon" src="/assets/rocket.png" className="w-40 sm:w-72" />
+						<div className="sm:w-20"></div>
+						<div className="flex flex-col justify-start items-start">
+							<div className="font-pixel text-2xl pb-2">For Players</div>
+							<div className="pb-3">
+								Find about latest games and get to experience them in the
+								nearest arena.
+							</div>
+							<div className="flex flex-row justify-center items-center pb-2">
+								<div className="w-4 h-4 rounded-xl bg-green-400"></div>
+								<div className="ps-2">Best equipments</div>
+							</div>
+							<div className="flex flex-row justify-center items-center pb-2">
+								<div className="w-4 h-4 rounded-xl bg-purple-400"></div>
+								<div className="ps-2">Friendly environment</div>
+							</div>
+							<div className="flex flex-row justify-center items-center pb-2">
+								<div className="w-4 h-4 rounded-xl bg-red-400"></div>
+								<div className="ps-2">Latest games</div>
+							</div>
+							<div className="flex flex-row justify-center items-center pb-2">
+								<div className="w-4 h-4 rounded-xl bg-orange-400"></div>
+								<div className="ps-2">Colab and compete</div>
+							</div>
+							<div className="pt-4"></div>
+							<StackButton
+								content="Book Now"
+								bgColor="bg-green-400"
+								textColor="text-black"
+								width="w-40"
+							/>
 						</div>
-						<div className="flex flex-row justify-center items-center pb-2">
-							<div className="w-4 h-4 rounded-xl bg-green-400"></div>
-							<div className="ps-2">Best equipments</div>
-						</div>
-						<div className="flex flex-row justify-center items-center pb-2">
-							<div className="w-4 h-4 rounded-xl bg-purple-400"></div>
-							<div className="ps-2">Friendly environment</div>
-						</div>
-						<div className="flex flex-row justify-center items-center pb-2">
-							<div className="w-4 h-4 rounded-xl bg-red-400"></div>
-							<div className="ps-2">Latest games</div>
-						</div>
-						<div className="flex flex-row justify-center items-center pb-2">
-							<div className="w-4 h-4 rounded-xl bg-orange-400"></div>
-							<div className="ps-2">Colab and compete</div>
-						</div>
-						<div className="pt-4"></div>
-						<StackButton
-							content="Book Now"
-							bgColor="bg-green-400"
-							textColor="text-black"
-							width="w-40"
-						/>
 					</div>
 				</StackContainer>
 			</div>
 
 			{/* Section Three */}
-			<div className="h-[80vh] w-[100vw]">
-				<div className="relative flex flex-col justify-center items-start w-[100vw] p-5 h-">
-					<img alt="icon" src="/assets/house.png" width="160" height="160" />
-
-					<div className="font-pixel text-2xl pb-2">For Arenas</div>
-					<div className="pb-3">
-						Become top rated hang-out place for gaming enthusiast and take your
-						business to the next level.
+			<div className="px-12">
+				<div className="relative flex flex-col sm:flex-row-reverse justify-center items-start w-[100vw]">
+					<img alt="icon" src="/assets/house.png" className="w-40 sm:w-72" />
+					<div className="sm:w-16"></div>
+					<div className="flex flex-col justify-start items-start">
+						<div className="font-pixel text-2xl pb-2">For Arenas</div>
+						<div className="pb-3 w-[50vw]">
+							Become top rated hang-out place for gaming enthusiast and take
+							your business to the next level.
+						</div>
+						<div className="flex flex-row justify-center items-center pb-2">
+							<div className="w-4 h-4 rounded-xl bg-green-400"></div>
+							<div className="ps-2">Get promoted</div>
+						</div>
+						<div className="flex flex-row justify-center items-center pb-2">
+							<div className="w-4 h-4 rounded-xl bg-purple-400"></div>
+							<div className="ps-2">Access to latest games</div>
+						</div>
+						<div className="flex flex-row justify-center items-center pb-2">
+							<div className="w-4 h-4 rounded-xl bg-red-400"></div>
+							<div className="ps-2">Manage booking digitally</div>
+						</div>
+						<div className="flex flex-row justify-center items-center pb-2">
+							<div className="w-4 h-4 rounded-xl bg-orange-400"></div>
+							<div className="ps-2">Transparent payments</div>
+						</div>
+						<div className="pt-4"></div>
+						<StackButton
+							content="Register Now!"
+							bgColor="bg-orange-400"
+							textColor="text-black"
+							width="w-48"
+						/>
 					</div>
-					<div className="flex flex-row justify-center items-center pb-2">
-						<div className="w-4 h-4 rounded-xl bg-green-400"></div>
-						<div className="ps-2">Get promoted</div>
-					</div>
-					<div className="flex flex-row justify-center items-center pb-2">
-						<div className="w-4 h-4 rounded-xl bg-purple-400"></div>
-						<div className="ps-2">Access to latest games</div>
-					</div>
-					<div className="flex flex-row justify-center items-center pb-2">
-						<div className="w-4 h-4 rounded-xl bg-red-400"></div>
-						<div className="ps-2">Manage booking digitally</div>
-					</div>
-					<div className="flex flex-row justify-center items-center pb-2">
-						<div className="w-4 h-4 rounded-xl bg-orange-400"></div>
-						<div className="ps-2">Transparent payments</div>
-					</div>
-					<div className="pt-4"></div>
-					<StackButton
-						content="Register Now!"
-						bgColor="bg-orange-400"
-						textColor="text-black"
-						width="w-48"
-					/>
 				</div>
 			</div>
 
 			{/* Section four */}
-			<div className="h-[70vh] w-screen z-20 flex items-start justify-center">
-				<StackContainer width="w-[90vw]" height="h-auto">
-					<div className="relative flex flex-col justify-center items-start w-[100vw] py-5 h-">
+			<div className="h-[90vh] w-screen z-20 flex items-center justify-center">
+				<StackContainer width="w-[90vw]" height="h-auto sm:h-[70vh]">
+					<div className="relative flex flex-col sm:flex-row justify-center items-start sm:items-center w-[100vw] py-5">
 						<img
 							alt="icon"
 							src="/assets/panda.png"
-							width="160"
-							height="160"
-							style={{ transform: 'scaleX(-1)' }}
+							className="w-40 transform -scale-x-100 sm:w-72"
 						/>
-
-						<div className="font-pixel text-2xl pb-2">Connect with us</div>
-						<div className="pb-3">
-							Let&apos;s connect and grow the gaming community together. Fill in
-							the details and we will get in touch with you.
+						<div className="sm:w-16"></div>
+						<div className="flex flex-col justify-center items-start sm:h-[60vh]">
+							<div className="font-pixel text-2xl pb-2">Connect with us</div>
+							<div className="pb-3">
+								Let&apos;s connect and grow the gaming community together. Fill
+								in the details and we will get in touch with you.
+							</div>
+							<div className="pt-4"></div>
+							<StackButton
+								content="Connect with us"
+								bgColor="bg-red-400"
+								textColor="text-black"
+								width="w-48 sm:w-52"
+							/>
 						</div>
-						<input
-							type="text"
-							className="bg-black border-2 placeholder-zinc-500 p-2 w-[80vw]"
-							placeholder="Phone or Email"
-						/>
-						<div className="pt-4"></div>
-						<StackButton
-							content="Connect with us"
-							bgColor="bg-red-400"
-							textColor="text-black"
-							width="w-48"
-						/>
 					</div>
 				</StackContainer>
 			</div>
--- src/components/stacked-button.tsx
+++ src/components/stacked-button.tsx
@@ -1,8 +1,8 @@
 const StackButton = ({content, bgColor, textColor, width}:{content:string, bgColor:string, textColor:string, width:string}) => (
 	<div className="relative">
-		<div className={`container ${width} h-12 bg-zinc-800 border-white border-2`}>
-			<div className={`container flex justify-center items-center ${width} h-12 ${bgColor} border-white border-2 mt-0.5 ml-0.5 py-4 px-4`}>
-        <div className={`text-m text-start ml-3 mr-3 ${textColor}`}>{content}</div>
+		<div className={`container ${width} h-12 sm:h-14 bg-zinc-800 border-white border-2`}>
+			<div className={`container flex justify-center items-center ${width} h-12 sm:h-14 ${bgColor} border-white border-2 mt-0.5 ml-0.5 py-4 px-4`}>
+        <div className={`text-m sm:text-xl  text-start ml-3 mr-3 ${textColor}`}>{content}</div>
       </div>
 		</div>
 	</div>
